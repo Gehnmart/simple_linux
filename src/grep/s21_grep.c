4b84f188f6a741d15f86b93d198f8bf85e746f89
Gehn Marty <gehnmart@pr-e4.nsk.21-school.ru>
2023-10-31 22:44:28 +0700

    lol_kek_cheburek

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -31,10 +31,12 @@ typedef struct {
   char** pattern;
 } Tpatterns;
 
-void regexp_import_from_file(char *path, Tpatterns* patterns);
+void add_pattern_from_file(char *path, Tpatterns* patterns);
 void parce(Tflags* flags, Tpatterns* patterns, int argc, char** argv);
 void scan_files(Tflags* flags, char** argv, bool cooked, Tpatterns* pattern);
 void only_pattern(FILE* file, Tpatterns* patterns, Tflags* flags, char* path, int file_counter);
+bool re_match(char* pattern, char* string, bool ignore_case);
+void add_pattern(Tpatterns* patterns, char* buf);
 
 int main(int argc, char** argv) {
 
@@ -68,12 +70,11 @@ void parce(Tflags* flags, Tpatterns* patterns, int argc, char** argv) {
     switch (res) {
       case 'e':
         flags->e_flag = flags->any_flag = True;
-        patterns->pattern[patterns->pattern_count] = malloc(sizeof(char) * 4098);
-        strcpy(patterns->pattern[patterns->pattern_count++], optarg);
+        add_pattern(patterns, optarg);
         break;
       case 'f':
         flags->f_flag = flags->any_flag = True;
-        regexp_import_from_file(optarg, patterns);
+        add_pattern_from_file(optarg, patterns);
         break;
       case 'i':
         flags->i_flag = flags->any_flag = True;
@@ -109,30 +110,47 @@ void parce(Tflags* flags, Tpatterns* patterns, int argc, char** argv) {
 void only_pattern(FILE* file, Tpatterns* patterns, Tflags* flags, char* path, int file_counter){
   char buf[4096];
 
-  //bool str_in = False;
-  (void)flags;
-  (void)path;
-  //int str_in_counter = 0;
+  int str_counter = 0;
 
   while((fgets(buf, 4096, file)) != NULL){
     char* ptrn;
-    int i = 0;
-    for(; (ptrn = patterns->pattern[i]) != NULL; i++){
-      regex_t reegex;
-  
-      int flag = 0;
-  
-      flag = regcomp( &reegex, ptrn, 0);
-      flag = regexec( &reegex, buf, 0, NULL, 0);
-
-      if(flags->v_flag) flag = !flag;
-      if(flag){
-        printf("%s", buf);
+    bool str_in = False;
+    for(int i = 0; ((ptrn = patterns->pattern[i]) != NULL); i++){
+      str_in = re_match(ptrn, buf, flags->i_flag);
+      if(str_in) break;
+    }
+    if(flags->v_flag) str_in = !str_in;
+    if(str_in){
+      if(flags->c_flag){
+        str_counter++;
+      }
+      else{
+        printf("%s%c%s",file_counter>1? path : "", ':', buf);
       }
     }
   }
-
+  if(flags->c_flag) printf("%s%c%d",file_counter>1? path : "", ':', str_counter);
   if(file_counter > 1) printf("%c", '\n');
+}
+
+bool re_match(char* pattern, char* string, bool ignore_case){
+  regex_t reegex;
+  int status;
+  
+  if(regcomp(&reegex, pattern, ignore_case ? REG_ICASE : 0) != 0) return 0;
+  
+  status = regexec(&reegex, string, 0, NULL, 0);
+
+  regfree(&reegex);
+
+  if (status != 0) return False;
+
+  return True;
+}
+
+void add_pattern(Tpatterns* patterns, char* buf){
+  patterns->pattern[patterns->pattern_count] = malloc(sizeof(char) * 4098);
+  strcpy(patterns->pattern[patterns->pattern_count++], buf);
 }
 
 void scan_files(Tflags* flags, char** argv, bool cooked, Tpatterns* patterns) {
@@ -143,8 +161,7 @@ void scan_files(Tflags* flags, char** argv, bool cooked, Tpatterns* patterns) {
   int i = 0;
 
   if((!flags->f_flag && !flags->e_flag) && argv[0] != NULL){
-    patterns->pattern[patterns->pattern_count] = malloc(sizeof(char) * 4098);
-    strcpy(patterns->pattern[patterns->pattern_count++], argv[0]);
+    add_pattern(patterns, argv[0]);
     file_counter--;
     i++;
   }
@@ -177,7 +194,7 @@ void scan_files(Tflags* flags, char** argv, bool cooked, Tpatterns* patterns) {
   }
 }
 
-void regexp_import_from_file(char *path, Tpatterns* patterns){
+void add_pattern_from_file(char *path, Tpatterns* patterns){
   FILE* file;
 
   if (path == NULL) {
@@ -190,8 +207,7 @@ void regexp_import_from_file(char *path, Tpatterns* patterns){
       char buf[4096];
       for(int i = 0; (fgets(buf, 4096, file)) != NULL; i++){
         if(buf[strlen(buf) - 1] == '\n') buf[strlen(buf) - 1] = '\0';
-        patterns->pattern[patterns->pattern_count] = malloc(sizeof(char) * 4098);
-        strcpy(patterns->pattern[patterns->pattern_count++], buf);
+        add_pattern(patterns, buf);
       }
     }
   }
